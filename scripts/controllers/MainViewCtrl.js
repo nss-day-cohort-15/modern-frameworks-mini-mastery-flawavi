34df6726ebfc4d30d77d7d33bb2466b6c926f6c9
Fletcher Watson <fbwthe6@gmail.com>
2016-09-13 10:06:45 -0500

    printed arrangements to the dom

--- scripts/controllers/MainViewCtrl.js
+++ scripts/controllers/MainViewCtrl.js
@@ -4,9 +4,11 @@ app.controller("MainViewCtrl", function($scope, FlowerFactory){
 
   $scope.message = "testing"
 
+
   $scope.getFlowers = function(){
     FlowerFactory.getFlowersFromFirebase()
     .then(function(data){
+
       $scope.flowers = data
       console.log($scope.flowers, "flowers data")
     })
